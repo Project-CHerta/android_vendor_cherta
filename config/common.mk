63d494f3dd7b14295ba6f965d0b2aa169afbdab2
charcoal4494 <charcoal4494@proton.me>
2022-08-20 04:34:14 +0200

    bootanimation: Import from Pixel
    Co-authored-by: AdarshGrewal <adarshgrewal@gmail.com>
    Co-authored-by: basamaryan <basam.aryan@gmail.com>
    Co-authored-by: hridaya <hridaya@pixelos.net>
    Co-authored-by: Karan Parashar <whyredfire@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -75,6 +75,17 @@ ifneq ($(TARGET_DISABLE_EPPE),true)
 $(call enforce-product-packages-exist-internal,$(lastword $(_include_stack)),product_manifest.xml rild Calendar android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package)
 endif
 
+# Bootanimation
+ifeq ($(strip $(TARGET_BOOT_ANIMATION_RES)),)
+    $(warning "TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+else
+    $(call soong_config_set,vendor_cherta,bootanimation_res,$(TARGET_BOOT_ANIMATION_RES))
+endif
+
+PRODUCT_PACKAGES += \
+    pixel_bootanimation \
+    pixel_bootanimation_dark
+
 # Google Photos Pixel Exclusive XML
 PRODUCT_COPY_FILES += \
     vendor/cherta/prebuilt/common/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
