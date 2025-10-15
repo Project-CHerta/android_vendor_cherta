42a515c5d3aa6099d7a3c43223d4a1ecd2a27923
Alberto Ponces <ponces26@gmail.com>
2025-10-15 07:06:06 +0530

    config: Switch to AOSPA's face unlock implementation
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I7b995ddb6fb54ab77e52adc18a5d76b790f566c0

--- config/common.mk
+++ config/common.mk
@@ -92,6 +92,20 @@ PRODUCT_PACKAGES += \
     ColumbusService
 endif
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
+
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    ParanoidSense
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 # Google Photos Pixel Exclusive XML
 PRODUCT_COPY_FILES += \
     vendor/cherta/prebuilt/common/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
