7534d3bc5a7e7bedf9c0a9e18910dd809494d442
Alberto Ponces <ponces26@gmail.com>
2026-01-22 17:37:26 +0700

    config: Switch to AOSPA's face unlock implementation
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: I7b995ddb6fb54ab77e52adc18a5d76b790f566c0

--- config/common.mk
+++ config/common.mk
@@ -148,6 +148,20 @@ ifneq ($(TARGET_DISABLE_EPPE),true)
 $(call enforce-product-packages-exist-internal,$(wildcard device/*/$(CHERTA_BUILD)/$(TARGET_PRODUCT).mk),product_manifest.xml rild Calendar android.hidl.memory@1.0-impl.vendor vndk_apex_snapshot_package)
 endif
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
+
+ifeq ($(TARGET_FACE_UNLOCK_SUPPORTED),true)
+PRODUCT_PACKAGES += \
+    ParanoidSense
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
+
 # GMS
 include vendor/cherta/config/pixel.mk
 
